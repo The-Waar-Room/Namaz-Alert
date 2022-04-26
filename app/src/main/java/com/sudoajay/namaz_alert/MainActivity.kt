68826f66cf71859445185afd19dbeadf803958a9
sudoajay <sudoajay@gmail.com>
2022-04-26 12:28:17 +0530

    Added firebase and tested

--- app/src/main/java/com/sudoajay/namaz_alert/MainActivity.kt
+++ app/src/main/java/com/sudoajay/namaz_alert/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.sudoajay.namaz_alert
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val nm:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M && !nm.isNotificationPolicyAccessGranted){
+//            startActivity(Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS))
+//        }
+//
+//        val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+//        am.ringerMode = AudioManager.RINGER_MODE_SILENT
     }
 }
